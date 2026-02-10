b033affb45f3b67ab2afb23dec00082b2c917091
DevuaIO <dev.devua@outlook.com>
2026-02-10 04:50:23 +0200

    feat: export all types of the library

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { ErrorSerializer } from "./ErrorSerializer";
 export { AxiosErrorPlugin } from "./plugins/AxiosErrorPlugin";
 export { StandardErrorPlugin } from "./plugins/StandardErrorPlugin";
 export { ZodErrorPlugin } from "./plugins/ZodErrorPlugin";
+
+export type { ErrorPlugin, SerializationCallback, ZodSerializationOptions, AppErrorResponse } from "./types";
